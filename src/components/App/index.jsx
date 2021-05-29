7686f7de70fef2755a5b24ccb6250cc866ed0cdd
Sarka Faloutova <sarka.faloutova@outlook.cz>
2021-05-29 10:07:43 +0200

    Component Reservation

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -2,11 +2,30 @@ import React from 'react';
 import { Footer } from '../Footer';
 import { Header } from '../Header';
 import { Home } from '../Home';
+import Reservation from '../Reservation';
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  NavLink,
+} from 'react-router-dom';
 
 export const App = () => (
   <>
     <Header />
-    <Home />
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+
+        <Route path="/reservation">
+          <Reservation />
+        </Route>
+      </Switch>
+    </Router>
     <Footer />
   </>
 );
---
+++ src/components/Reservation/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Reservation = () => {
+  return <h2>Detail jizdenky</h2>;
+};
+
+export default Reservation;
